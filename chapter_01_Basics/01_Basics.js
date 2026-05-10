c83a0963e5cfe57d715eda5704e65f8407222a30
Supriya <thombaresupriya05@gmail.com>
2026-05-10 07:19:42 +0530

    day2 learning

---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,12 @@
+console.log("Supriya wel come to testing Accadmy");
+let x=10;
+
+for(let a=0; a<1000; a++)
+{
+    console.log(x);
+    print();
+}
+
+function print() {
+    console.log("Hello");ssssss
+}
